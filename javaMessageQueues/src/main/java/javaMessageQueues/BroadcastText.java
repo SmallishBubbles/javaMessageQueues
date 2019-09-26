2246ef8b4ad1d7f3f94cdb03d9099f6b06888dfa
cmcoulon <ccoulon@amazon.com>
2019-09-26 13:42:20 -0700

    completed lab 39

---
+++ javaMessageQueues/src/main/java/javaMessageQueues/BroadcastText.java
@@ -0,0 +1,15 @@
+package javaMessageQueues;
+
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import javaMessageQueues.models.Task;
+
+public class BroadcastText {
+
+    public void broadcastText(Task jsonObject) {
+        AmazonSNS sns = AmazonSNSClient.builder().build();
+
+        String messageToBroadcast
+    }
+
+}
---
+++ javaMessageQueues/src/main/java/javaMessageQueues/Number.java
@@ -0,0 +1,18 @@
+package javaMessageQueues;
+
+public class Number {
+    public String number;
+
+    public Number () {}
+    public Number (String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
